07a64dcafd49a750b53651e7405968ad1a02c168
Akshay Anand <akshayan@thoughtworks.com>
2020-01-19 17:48:22 +0800

    Initial structure

---
+++ src/test/java/com/akshay/testrmm/TestRmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.testrmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
